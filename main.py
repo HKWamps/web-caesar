3d3be315cecc9b9c1a982e3d749c539697a8b0f8
Mike Wampler <mikewampler@gmail.com>
2018-12-03 02:16:42 -0500

    more changes

--- main.py
+++ main.py
@@ -2,8 +2,43 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """ 
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form action="/add" method="post">
+        <label for="new-movie">
+            Rotate by: 
+            <input type="text" id="rot" name="rot" value ="0"/ ><br><br>
+            <input type="textarea" id="text" name="text"/><br><br>
+        </label>
+        <input type="submit" value="Submit Query"/>
+    </form>
+    
+    </body>
+</html>"""
+
 @app.route("/")
 def index():
-    return "Hellow World"
+    return "Hello World"
 
 app.run()
